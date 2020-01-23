7deff88606b5937e76ae28905338ee32deeac731
Rayees khan <55346459+Rayeeskha@users.noreply.github.com>
2020-01-23 11:36:01 -0800

    Add files via upload

---
+++ EMPLOYEE_ATTENDANCE_SYSTEM_IN_PHP_WITH_SOURCE_CODE/student-attendance/inc/footer.php
@@ -0,0 +1,18 @@
+	<div class="container mt-3">
+		<div class="card card-body bg-light text-center">
+			<h3>
+				<span>Copyright &copy;</span>
+				<script type="text/javascript">
+					var d = new Date();
+					document.write(d.getFullYear());
+				</script>
+				<span>
+					<a target="_blank" href="https://khanrayees.000webhostapp.com">
+						Khan Rayees
+					</a>
+				</span>
+			</h3>
+		</div>
+	</div>
+</body>
+</html>
